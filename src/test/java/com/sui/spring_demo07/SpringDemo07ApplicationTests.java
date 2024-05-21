78ae0cb4d4979cfb5d4ceb68dfa026f296b5dd08
suijiyuan <591858983@qq.com>
2024-05-21 22:07:36 +0800

    初始化

---
+++ src/test/java/com/sui/spring_demo07/SpringDemo07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sui.spring_demo07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemo07ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
